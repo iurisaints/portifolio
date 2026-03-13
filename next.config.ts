5a3732360d2b1217322c308548d8f8c46a494213
Iuri Santos <50501178+iurisaints@users.noreply.github.com>
2026-03-13 08:32:22 -0300

    Remove output configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   basePath: '/portifolio',
   images: {
     unoptimized: true,
